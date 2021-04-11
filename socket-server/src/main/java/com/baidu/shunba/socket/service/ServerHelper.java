7202ea2890d9affab1af1f12fa214bcf86ec8342
Chris <Z0287yy@gmail.com>
2021-04-11 13:03:26 +0800

    update log

--- socket-server/src/main/java/com/baidu/shunba/socket/service/ServerHelper.java
+++ socket-server/src/main/java/com/baidu/shunba/socket/service/ServerHelper.java
@@ -26,12 +26,15 @@ public class ServerHelper {
 
     public static MessageJson sendMessageToOne(SystemService systemService, String deviceId, ResponseBean message) {
         try {
+            logger.info("SocketSever接收到推送到指定设备[" + deviceId + "]的消息: " + message);
             ChannelHandlerContext context = ServerHandler.onlineContexts.get(deviceId);
             if (context == null) {
+                logger.info("SocketSever接收到推送到指定设备[" + deviceId + "]的消息，未找到设备: " + message);
                 return MessageJson.newInstanceWithError("未找到连接设备");
             }
 
             context.writeAndFlush(message + ServerHandler.ResponseSplit);
+            logger.info("SocketSever接收到推送到指定设备[" + deviceId + "]的消息已完成: " + message);
         } catch (Exception e) {
             logger.error("sendMessageToShift", e);
             return MessageJson.newInstanceWithError(e);
@@ -42,11 +45,13 @@ public class ServerHelper {
 
     public static MessageJson sendMessageToAll(SystemService systemService, ResponseBean message) {
         try {
+            logger.info("SocketSever接收到推送到所有的消息: " + message);
             for (ChannelHandlerContext context : ServerHandler.onlineContexts.values()) {
                 if (context != null) {
                     context.writeAndFlush(message + ServerHandler.ResponseSplit);
                 }
             }
+            logger.info("SocketSever接收到推送到所有的消息，完成推送: " + message);
         } catch (Exception e) {
             logger.error("sendMessageToShift", e);
             return MessageJson.newInstanceWithError(e);
@@ -57,21 +62,23 @@ public class ServerHelper {
 
 
     public static MessageJson sendMessageToShift(SystemService systemService, String shiftNo, ResponseBean message) {
-        String json = new Gson().toJson(message);
         try {
-            logger.info("接收到班次: [" + shiftNo + "]的票据更新任务: " + json);
+            logger.info("SocketSever接收到班次: [" + shiftNo + "]的新任务: " + message);
             List<Map<String, Object>> results = systemService.findForJdbc("select device_id as deviceId from sb_device where shift_no = '" + shiftNo + "'");
             if (results.size() == 0) {
-                logger.error("根据班次: [" + shiftNo + "]未查到已发车设备, 无法发送更新票据消息: " + json);
+                logger.error("SocketSever根据班次: [" + shiftNo + "]未查到已发车设备, 无法发送消息: " + message);
             }
             for (Map<String, Object> map : results) {
                 String deviceId = ObjectUtils.getString(map.get("deviceId"));
-                logger.info("根据班次: [" + shiftNo + "]查到已发车设备: [" + deviceId + "], 开始推送更新票据消息: " + json);
+
                 ChannelHandlerContext context = ServerHandler.onlineContexts.get(deviceId);
                 if (context != null) {
+                    logger.info("SocketSever根据班次: [" + shiftNo + "]查到已发车设备: [" + deviceId + "], 推送消息，开始，: " + message);
                     context.writeAndFlush(message + ServerHandler.ResponseSplit);
+                    logger.info("SocketSever根据班次: [" + shiftNo + "]查到已发车设备: [" + deviceId + "], 推送消息，完成，" + message);
                 }
             }
+            logger.info("SocketSever接收到班次: [" + shiftNo + "]的新任务已完成: " + message);
         } catch (Exception e) {
             logger.error("sendMessageToShift", e);
             return MessageJson.newInstanceWithError(e);
@@ -82,13 +89,17 @@ public class ServerHelper {
     public static MessageJson sendMessageToLine(SystemService systemService, String lineId, ResponseBean message) {
         try {
             List<Map<String, Object>> results = systemService.findForJdbc("select device_id as deviceId from sb_device where line_id = '" + lineId + "'");
+            logger.info("SocketSever接收到线路: [" + lineId + "]的票据更新任务，数据库返回设备数：[" + results.size() + "]: " + message);
             for (Map<String, Object> map : results) {
                 String deviceId = ObjectUtils.getString(map.get("deviceId"));
                 ChannelHandlerContext context = ServerHandler.onlineContexts.get(deviceId);
                 if (context != null) {
+                    logger.info("SocketSever根据线路: [" + lineId + "]查到已发车设备: [" + deviceId + "], 推送消息，开始，: " + message);
                     context.writeAndFlush(message + ServerHandler.ResponseSplit);
+                    logger.info("SocketSever根据线路: [" + lineId + "]查到已发车设备: [" + deviceId + "], 推送消息，开始，: " + message);
                 }
             }
+            logger.info("SocketSever接收到线路: [" + lineId + "]的票据更新任务，已完成: " + message);
         } catch (Exception e) {
             logger.error("sendMessageToLine", e);
             return MessageJson.newInstanceWithError(e);
@@ -156,7 +167,9 @@ public class ServerHelper {
             content.serialNumber = device.getSerialNumber().getSerialNumber();
             response.setContent(content);
 
+            logger.info("SocketSever推送注册信息到设备[" + device.getDeviceId() + "]，开始: " + response);
             context.writeAndFlush(response + ServerHandler.ResponseSplit);
+            logger.info("SocketSever推送注册信息到设备[" + device.getDeviceId() + "]，完成: " + response);
         } catch (Exception e) {
             logger.error("pushAuthToDevice", e);
             return MessageJson.newInstanceWithError(e);
